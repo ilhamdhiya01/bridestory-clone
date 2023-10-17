eb8e9e418957eedfaef3872ec6ea746a27841b99
Ilham Dhiya Ulhaq <ilham.dhiya@runsystem.id>
2023-10-17 16:45:58 +0700

    add vendor category and add modal

--- app/components/index.d.ts
+++ app/components/index.d.ts
@@ -5,3 +5,17 @@ export type MenuItemProps = {
   icon: IconType;
   link: string;
 };
+
+export type ModalProps = {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  header?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: string;
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
 import Navbar from './components/navbar/Navbar';
 import Menu from './components/navbar/Menu';
+import Modal from './components/modal/Modal';
 
 const figtree = Figtree({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <body className={`${figtree.className} w-full min-h-screen box-border`}>
         <>
+          <Modal />
           <Navbar />
           <Menu />
         </>
